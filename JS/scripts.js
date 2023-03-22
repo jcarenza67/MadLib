ef74fd726c7d3d8de2337b939186ce15deca1a41
Joseph Wilfong <josephwilfong91@gmail.com>
2023-03-22 09:39:42 -0700

    add global tags back into js.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,27 +1,27 @@
-onload = function() {
+window.onload = function() {
     // first we set up an event handler for the form submission
-    let form = querySelector("form");
+    let form = document.querySelector("form");
     form.onsubmit = function(event) {
-    const person1Input = getElementById("person1Input").value;
-    const person2Input = getElementById("person2Input").value;
-    const animalInput= getElementById("animalInput").value;
-    const exclamationInput = getElementById("exclamationInput").value;
-    const verbInput = getElementById("verbInput").value;
-    const nounInput = getElementById("nounInput").value;
+    const person1Input = document.getElementById("person1Input").value;
+    const person2Input = document.getElementById("person2Input").value;
+    const animalInput= document.getElementById("animalInput").value;
+    const exclamationInput = document.getElementById("exclamationInput").value;
+    const verbInput = document.getElementById("verbInput").value;
+    const nounInput = document.getElementById("nounInput").value;
       // then we print values to the story area;
       // we're hardcoding these values for now
-    querySelector("span#person1a").innerText = person1Input;
-    querySelector("span#person1b").innerText = person1Input;
-    querySelector("span#person1c").innerText = person1Input;
-    querySelector("span#person2a").innerText = person2Input;
-    querySelector("span#person2b").innerText = person2Input;
-    querySelector("span#animal").innerText = animalInput;
-    querySelector("span#verb").innerText = verbInput;
-    querySelector("span#noun").innerText = nounInput;
-    querySelector("span#exclamation").innerText = exclamationInput;
+    document.querySelector("span#person1a").innerText = person1Input;
+    document.querySelector("span#person1b").innerText = person1Input;
+    document.querySelector("span#person1c").innerText = person1Input;
+    document.querySelector("span#person2a").innerText = person2Input;
+    document.querySelector("span#person2b").innerText = person2Input;
+    document.querySelector("span#animal").innerText = animalInput;
+    document.querySelector("span#verb").innerText = verbInput;
+    document.querySelector("span#noun").innerText = nounInput;
+    document.querySelector("span#exclamation").innerText = exclamationInput;
 
       // then we show the story by removing the class attribute
-    querySelector("div#story").removeAttribute("class");
+      document.querySelector("div#story").removeAttribute("class");
     event.preventDefault();
     };
   };
